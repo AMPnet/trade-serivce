cd99ddb86f7a85e7c999e416e58f087fa2a57f77
Eugen Druzin <edruzin@gmail.com>
2021-10-22 14:06:56 +0200

    Add task tables

--- src/main/resources/db/migration/V2__polling_task.sql
+++ src/main/resources/db/migration/V2__polling_task.sql
@@ -1,6 +1,42 @@
 -- Task
-CREATE TABLE task(
+CREATE TABLE task
+(
     chain_id     BIGINT PRIMARY KEY,
     block_number BIGINT NOT NULL,
     timestamp    BIGINT NOT NULL
+);
+
+CREATE TYPE task_status AS ENUM ('PENDING', 'SUCCESSFUL', 'FAILED');
+CREATE TYPE task_type AS ENUM ('BUY', 'SELL');
+
+CREATE TABLE blockchain_task
+(
+    id            SERIAL UNIQUE,
+    wallet        VARCHAR                  NOT NULL,
+    chain_id      BIGINT                   NOT NULL,
+    amount_usd    BIGINT                   NOT NULL,
+    order_id      BIGINT                   NOT NULL,
+    type          task_type                NOT NULL,
+    status        task_status              NOT NULL,
+    created_at    TIMESTAMP WITH TIME ZONE NOT NULL,
+    hash          VARCHAR,
+    order_task_id BIGINT,
+    updated_at    TIMESTAMP WITH TIME ZONE,
+    CONSTRAINT uc_bt_wallet_order UNIQUE (wallet, order_id)
+);
+
+CREATE TABLE order_task
+(
+    id                 SERIAL UNIQUE,
+    wallet             VARCHAR                  NOT NULL,
+    amount_usd         BIGINT                   NOT NULL,
+    order_id           BIGINT                   NOT NULL,
+    stock_id           BIGINT                   NOT NULL,
+    type               task_type                NOT NULL,
+    status             task_status              NOT NULL,
+    created_at         TIMESTAMP WITH TIME ZONE NOT NULL,
+    confirmation_id    VARCHAR,
+    blockchain_task_id BIGINT,
+    updated_at         TIMESTAMP WITH TIME ZONE,
+    CONSTRAINT uc_ot_wallet_order UNIQUE (wallet, order_id)
 );
